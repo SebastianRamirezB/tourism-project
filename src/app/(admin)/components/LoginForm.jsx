8c1535f4398e076a477396c8f545ebc9fad480cf
Sebastian Ramirez <sebastianramirez748@gmail.com>
2024-01-21 17:10:27 -0500

    logic is added for user startup and registration

--- src/app/(admin)/components/LoginForm.jsx
+++ src/app/(admin)/components/LoginForm.jsx
@@ -1,10 +1,59 @@
+'use client';
+import { useForm } from '@/hooks/useForm';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export const LoginForm = () => {
+  const router = useRouter();
+  const {
+    formState,
+    onInputChange
+  } = useForm({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formState;
+
+  const login = async (e) => {
+    e.preventDefault();
+
+    const data = await fetch('http://localhost:3001/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    });
+    const user = await data.json();
+
+    if (!user.token) return;
+
+    router.push('/dashboard');
+  };
+
   return (
-    <form className="flex flex-col gap-8">
-        <input className=" text-xl py-6 px-3 border-b border-[#B4B4B4] outline-none" type="email" placeholder="Email" />
-        <input className=" text-xl py-6 px-3 border-b border-[#B4B4B4] outline-none" type="password" placeholder="Contraseña" />
+    <form className="flex flex-col gap-8" onSubmit={login}>
+        <input
+          name="email"
+          className=" text-xl py-6 px-3 border-b border-[#B4B4B4] outline-none"
+          type="email"
+          placeholder="Email"
+          onChange={onInputChange}
+          value={email}
+        />
+        <input
+          name="password"
+          className=" text-xl py-6 px-3 border-b border-[#B4B4B4] outline-none"
+          type="password"
+          placeholder="Contraseña"
+          onChange={onInputChange}
+          value={password}
+
+        />
 
         <button className="bg-primary capitalize py-6 px-10 w-full text-2xl text-white rounded-[45px] font-bold" >iniciar sesión</button>
 
--- src/app/(admin)/components/RegisterForm.jsx
+++ src/app/(admin)/components/RegisterForm.jsx
@@ -1,12 +1,80 @@
+'use client';
+import { useForm } from '@/hooks/useForm';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export const RegisterForm = () => {
+  const router = useRouter();
+  const {
+    formState,
+    onInputChange
+  } = useForm({
+    fullName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const { fullName, email, password, confirmPassword } = formState;
+
+  const register = async (e) => {
+    e.preventDefault();
+
+    const data = await fetch('http://localhost:3001/api/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password,
+        fullName
+      })
+    });
+    const user = await data.json();
+    console.log(user);
+    if (!user.token) return;
+
+    router.push('/dashboard');
+  };
+
   return (
-    <form className="flex flex-col gap-8">
-      <input className=" text-xl py-6 px-3 border-b border-[#B4B4B4] outline-none" type="text" placeholder="Nombre" />
-      <input className=" text-xl py-6 px-3 border-b border-[#B4B4B4] outline-none" type="email" placeholder="Email" />
-      <input className=" text-xl py-6 px-3 border-b border-[#B4B4B4] outline-none" type="password" placeholder="Contraseña" />
-      <input className=" text-xl py-6 px-3 border-b border-[#B4B4B4] outline-none" type="password" placeholder="Confirmar Contraseña" />
+    <form className="flex flex-col gap-8" onSubmit={register}>
+      <input
+        name="fullName"
+        className=" text-xl py-6 px-3 border-b border-[#B4B4B4] outline-none"
+        type="text"
+        placeholder="Nombre"
+        onChange={onInputChange}
+        value={fullName}
+      />
+      <input
+        name="email"
+        className=" text-xl py-6 px-3 border-b border-[#B4B4B4] outline-none"
+        type="email"
+        placeholder="Email"
+        onChange={onInputChange}
+        value={email}
+
+      />
+      <input
+        name="password"
+        className=" text-xl py-6 px-3 border-b border-[#B4B4B4] outline-none"
+        type="password"
+        placeholder="Contraseña"
+        onChange={onInputChange}
+        value={password}
+
+      />
+      <input
+        name="confirmPassword"
+        className=" text-xl py-6 px-3 border-b border-[#B4B4B4] outline-none"
+        type="password"
+        placeholder="Confirmar Contraseña"
+        onChange={onInputChange}
+        value={confirmPassword}
+
+      />
 
       <button className="bg-primary capitalize py-6 px-10 w-full text-2xl text-white rounded-[45px] font-bold" >Crear Cuenta</button>
 
